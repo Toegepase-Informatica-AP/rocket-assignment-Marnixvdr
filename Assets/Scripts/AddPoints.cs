cf084a64fd40c4625b5de851f2ce65e75cb56162
unknown <marnixvdr@gmail.com>
2020-10-12 13:24:52 +0200

    Adding files

---
+++ Assets/Scripts/AddPoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddPoints : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            Score.scoreValue += 1;
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+    string LevelToLoad = "Level 2" ;
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(LevelToLoad);
+        }
+
+    }
+}
